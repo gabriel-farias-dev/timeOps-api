8b42f7ffc66eb4ed628ed3fdfbeaa86e0ae701b8
gabriel-farias-dev <gabrielfarias.dev@gmail.com>
2026-03-18 22:12:55 -0300

    FEAT: Novo controller de cadastro de usuario

--- src/app.js
+++ src/app.js
@@ -16,4 +16,3 @@ const PORT = 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-project_id;
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 const prisma = require("../prismaClient");
+const bcrypt = require("bcrypt");
 
 const getUsers = async (req, res) => {
   try {
@@ -10,4 +11,45 @@ const getUsers = async (req, res) => {
   }
 };
 
-module.exports = { getUsers };
+const setUser = async (req, res) => {
+  try {
+    const { name, email, password, role, active } = req.body;
+
+    //Preciso checar agora se o email e a senha estão preenchidos,
+    //eles são precisos para que aconteça a inserção do dado
+    if (!email || !password) {
+      return res.status(400).json({ erros: "Email e Senha são obrigatorios" });
+    }
+
+    // Estou fazendo com que a senha do usuario não seja gravada dentro do banco de dados
+    const encrptPwd = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.login_User.create({
+      data: {
+        name,
+        email,
+        encrptPwd,
+        role,
+        active,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getUser = (req,res) => {
+  const {email} = req.body;
+
+  const user = async prisma.findUnique
+}
+
+// id Int @id @default(autoincrement())
+// name String
+// email String
+// password String
+// role String
+// active Boolean
+
+module.exports = { getUsers, setUser };
